d010b6ccf5ebc04897830ba52b15ff44617f6a4a
MA Ahi <bijoyrashidul21@gmail.com>
2026-01-14 20:55:25 +0600

    initial comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "./components/ui/toaster";
 import { TooltipProvider } from "./components/ui/tooltip";
-
 import Home from "./pages/Home";
 import Biography from "./pages/Biography";
 import Gallery from "./pages/Gallery";
